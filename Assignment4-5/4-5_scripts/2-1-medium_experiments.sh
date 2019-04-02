2d8ea1cfe9695a86adce914a6ef0bd6da8f11855
LeeH17 <LeeH17@rpi.edu>
2019-04-02 11:00:26 -0400

    Outputs updated for 128 nodes, runtimes csv included

--- Assignment4-5/4-5_scripts/2-1-medium_experiments.sh
+++ Assignment4-5/4-5_scripts/2-1-medium_experiments.sh
@@ -5,6 +5,14 @@
 #sbatch --partition medium --nodes 128 --time 15 barn/4-5_scripts/2-1-medium_experiments.sh
 BARN=/gpfs/u/barn/PCP8/PCP8lhrr
 SCRATCH=/gpfs/u/scratch/PCP8/PCP8lhrr
+
+#Clean up output files to ensure consistent running
+rm $SCRATCH/2-1_64r-4t_io_output
+rm $SCRATCH/2-1_16r4t_io_output
+rm $SCRATCH/2-1_4r-16t_io_output
+rm $SCRATCH/2-1_2r-32t_io_output
+rm $SCRATCH/2-1_1r-64t_io_output
+
 srun -N128 --ntasks-per-node=64 --overcommit -o $SCRATCH/2-1_128nodes-256ranks-1thread.log $BARN/assignment4-5 1 0.25 256 $SCRATCH/2-1_64r-4t_io_output
 srun -N128 --ntasks-per-node=16 --overcommit -o $SCRATCH/2-1_128nodes-64ranks-4thread.log $BARN/assignment4-5 4 0.25 256 $SCRATCH/2-1_16r4t_io_output
 srun -N128 --ntasks-per-node=4 --overcommit -o $SCRATCH/2-1_128nodes-16ranks-16thread.log $BARN/assignment4-5 16 0.25 256 $SCRATCH/2-1_4r-16t_io_output
