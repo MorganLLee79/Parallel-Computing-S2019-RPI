ef45ce939ac131a69a96f138d63df495d444bb7d
LeeH17 <LeeH17@rpi.edu>
2019-04-25 21:55:22 -0400

    minor & thing in scripts

--- Project/testing_scripts/64_rank_experiments_real_graphs.sh
+++ Project/testing_scripts/64_rank_experiments_real_graphs.sh
@@ -8,11 +8,11 @@ BARN=/gpfs/u/barn/PCP8/PCP8lhrr
 SCRATCH=/gpfs/u/scratch/PCP8/PCP8lhrr
 EXECUTABLE=$SCRATCH/project.out
 srun -N64 --ntasks-per-node=1 --overcommit $EXECUTABLE $SCRATCH/TestGraphs/roadnet-PA.adj 64 > $SCRATCH/64_rank_results/64r-PA.log &
-srun -N64 --ntasks-per-node=1 --overcommit $EXECUTABLE $SCRATCH/TestGraphs/roadnet-TX.adj 64 > $SCRATCH/64_rank_results/64r-TX.log
+srun -N64 --ntasks-per-node=1 --overcommit $EXECUTABLE $SCRATCH/TestGraphs/roadnet-TX.adj 64 > $SCRATCH/64_rank_results/64r-TX.log &
 wait
 
 srun -N64 --ntasks-per-node=1 --overcommit $EXECUTABLE $SCRATCH/TestGraphs/roadnet-CA.adj 64 > $SCRATCH/64_rank_results/64r-CA.log &
-srun -N64 --ntasks-per-node=1 --overcommit $EXECUTABLE $SCRATCH/TestGraphs/social-slashdot.adj 64 > $SCRATCH/64_rank_results/64r-social.log
+srun -N64 --ntasks-per-node=1 --overcommit $EXECUTABLE $SCRATCH/TestGraphs/social-slashdot.adj 64 > $SCRATCH/64_rank_results/64r-social.log &
 wait
 
 srun -N64 --ntasks-per-node=1 --overcommit $EXECUTABLE $SCRATCH/TestGraphs/web-google.adj 64 > $SCRATCH/64_rank_results/64r-webcrawl.log
